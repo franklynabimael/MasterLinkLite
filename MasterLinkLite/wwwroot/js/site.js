11cffc820484df40c49c99db7b0098887b08d52a
fr23178 <fr23178@CVTL-PF24MTKH>
2025-07-15 23:47:15 -0400

    filtro a administrador de enlaces, borrado de usuario, grid a la vista de enlaces desde administrador enlaces

--- MasterLinkLite/wwwroot/js/site.js
+++ MasterLinkLite/wwwroot/js/site.js
@@ -2,3 +2,21 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    function filtrarTabla() {
+            const input = document.getElementById("filtro");
+    const filter = input.value.toLowerCase();
+    const rows = document.querySelectorAll("table tbody tr");
+
+            rows.forEach(row => {
+                const nombre = row.cells[0].innerText.toLowerCase();
+    const proposito = row.cells[1].innerText.toLowerCase();
+
+    if (nombre.includes(filter) || proposito.includes(filter)) {
+        row.style.display = "";
+                } else {
+        row.style.display = "none";
+                }
+            });
+        }
+
